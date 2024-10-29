2972d2561ebbcee3adf8cde6f50c7c4a9aaf72f7
guadaluperial <guadaluperial1234@gmail.com>
2024-10-29 13:35:21 -0300

    Agrego la funcionalidad makeReservation en el CustomerService, junto con un screeing repo y un reservation repo, para verificar si existen previamente dichas cosas

--- backend/src/main/java/com/um/edu/uy/repository/ReservationRepository.java
+++ backend/src/main/java/com/um/edu/uy/repository/ReservationRepository.java
@@ -1,11 +1,10 @@
 package com.um.edu.uy.repository;
 
-import com.um.edu.uy.entities.Customer;
-import com.um.edu.uy.entities.Reservation;
-import com.um.edu.uy.entities.ReservationId;
-import com.um.edu.uy.entities.ScreeningID;
+import com.um.edu.uy.entities.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReservationRepository extends JpaRepository<Reservation, ReservationId> {
-    //public Reservation findById(Integer col, Integer row, ScreeningID screening);
+    Optional<Reservation> findByScreeningAndColAndRow(Screening screening, Integer col, Integer row);
 }
--- backend/src/main/java/com/um/edu/uy/services/CustomerService.java
+++ backend/src/main/java/com/um/edu/uy/services/CustomerService.java
@@ -1,9 +1,13 @@
 package com.um.edu.uy.services;
 
 import com.um.edu.uy.entities.Customer;
+import com.um.edu.uy.entities.Reservation;
+import com.um.edu.uy.entities.Screening;
 import com.um.edu.uy.entities.User;
 import com.um.edu.uy.exceptions.InvalidDataException;
 import com.um.edu.uy.repository.CustomerRepository;
+import com.um.edu.uy.repository.ReservationRepository;
+import com.um.edu.uy.repository.ScreeningRepository;
 import com.um.edu.uy.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +22,12 @@ public class CustomerService {
 
     @Autowired
     UserRepository userRepo;
+    @Autowired
+    private ScreeningRepository screeningRepo;
+
+    @Autowired
+    private ReservationRepository reservationRepo;
+
 
     public List<Customer> getAll() {return customerRepo.findAll();}
 
@@ -32,6 +42,36 @@ public class CustomerService {
         } else {
             throw new InvalidDataException("Usuario no encontrado");
         }
+    }
+    public Reservation makeReservation(String email, Integer col, Integer row, Screening screening) throws InvalidDataException {
+        Optional<Customer> customerOpt = customerRepo.findById(email);
+        if (customerOpt.isEmpty()) {
+            throw new InvalidDataException("Cliente no encontrado");
+        }
+        Customer customer = customerOpt.get();
+
+        Optional<Screening> screeningOpt = screeningRepo.findById(screening. /////);
+        if (screeningOpt.isEmpty()) {
+            throw new InvalidDataException("La función especificada no existe");
+        }
+
+        Optional<Reservation> existingReservation = reservationRepo.findByScreeningAndColAndRow(screening, col, row);
+        if (existingReservation.isPresent()) {
+            throw new InvalidDataException("El asiento ya está reservado");
+        }
+
+        Reservation reservation = Reservation.builder()
+                .customer(customer)
+                .screening(screening)
+                .col(col)
+                .row(row)
+                .build();
+
+        reservationRepo.save(reservation);
+        customer.getReservations().add(reservation);
+        customerRepo.save(customer);
+
+        return reservation;
     }
 
 }
